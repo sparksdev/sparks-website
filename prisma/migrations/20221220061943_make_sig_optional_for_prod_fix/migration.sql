c2c86e28daace81248c83f8fd5e4ef52c3149cc2
sparksdev <dev@sparks.dev>
2022-12-20 01:39:42 -0500

    add signature

--- prisma/migrations/20221220061943_make_sig_optional_for_prod_fix/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "DeployerProfile" ALTER COLUMN "signature";
--- prisma/migrations/20221220063926_add_sig/migration.sql
+++ prisma/migrations/20221220063926_add_sig/migration.sql
@@ -2,7 +2,6 @@
   Warnings:
 
   - A unique constraint covering the columns `[signature]` on the table `DeployerProfile` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `signature` to the `DeployerProfile` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
