e29735e47386533dcb452d1605072958de28ace7
sparksdev <dev@sparks.dev>
2023-04-07 10:48:13 -0400

    redirect to foundation

--- middleware.js
+++ middleware.js
@@ -3,19 +3,20 @@ import { NextResponse } from 'next/server'
 export const rewrites = []
 
 export const redirects = [
-  { subdomain: 'linktree', href: `${process.env.ORIGIN}/#links` },
-  { subdomain: 'links', href: `${process.env.ORIGIN}/#links` },
-  { subdomain: 'discord', href: 'https://discord.com/invite/JuNWR6vGKC' },
-  { subdomain: 'twitter', href: 'https://twitter.com/sparksdev_' },
-  { subdomain: 'github', href: 'https://github.com/sparksdev' },
-  { subdomain: 'gitcoin', href: 'https://gitcoin.co/sparksdev' },
-  { subdomain: 'medium', href: 'https://medium.com/@sparksdev' },
-  { subdomain: 'telegram', href: 'https://t.me/sparks_official' },
-  { subdomain: 'notion', href: 'https://sparksdev.notion.site/sparksdev/SPARKS-b3450f9c62d74923960f69eece7be6c3' },
+  { subdomain: 'root', href: 'https://sparks.foundation' },
+  { subdomain: 'linktree', href: `https://links.sparks.foundation` },
+  { subdomain: 'links', href: `https://links.sparks.foundation` },
+  { subdomain: 'discord', href: 'https://discord.sparks.foundation' },
+  { subdomain: 'twitter', href: 'https://twitter.sparks.foundation_' },
+  { subdomain: 'github', href: 'https://github.sparks.foundation' },
+  { subdomain: 'gitcoin', href: 'https://gitbook.sparks.foundation' },
+  { subdomain: 'medium', href: 'https://gitbook.sparks.foundation' },
+  { subdomain: 'telegram', href: 'https://telegram.sparks.foundation' },
+  { subdomain: 'notion', href: 'https://gitbook.sparks.foundation' },
 ]
 
 export function middleware(req) {
-  const subdomain = req.headers.get('host').split('.')[0]
+  const subdomain = req.headers.get('host').split('.')[0] || 'root'
   const redirect = redirects.find((r) => r.subdomain === subdomain)
   if (redirect) return NextResponse.redirect(redirect.href, 308)
   const rewrite = rewrites.find((r) => r.subdomain === subdomain)
